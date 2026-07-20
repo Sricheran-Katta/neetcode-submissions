68615d4d2d81aaa4b87002e071172153dfb337f1
Sricheran Katta <sricherank@iisc.ac.in>
2026-07-20 11:03:36 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,37 @@
+class UnionFind{
+private:
+    vector<int> parent;
+public:
+    UnionFind(int n){
+        parent.resize(n+1);
+        for(int i = 0; i < n + 1; i++){
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }   
+    bool unite(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY)return false;
+        parent[rootY] = rootX;
+        return true; 
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        UnionFind dsu(n);
+        for(auto& edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            if(!dsu.unite(u, v)){
+                return {u, v};
+            }
+        }
+        return {};
+    }
+};
